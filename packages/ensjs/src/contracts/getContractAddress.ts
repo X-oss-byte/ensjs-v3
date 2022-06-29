a38229b31a2235ad90103556d1e47b5a4415c568
tate <tate@ens.domains>
2022-06-29 10:34:38 +1000

    add goerli universalresolver address

--- packages/ensjs/src/contracts/getContractAddress.ts
+++ packages/ensjs/src/contracts/getContractAddress.ts
@@ -33,6 +33,7 @@ const addresses: Record<
   UniversalResolver: {
     '1': '0x580AF46E06DaaD47eb5940526FD64d95b815Cb70',
     '4': '0x74e20bd2a1fe0cdbe45b9a1d89cb7e0a45b36376',
+    '5': '0x74E20Bd2A1fE0cdbe45b9A1d89cb7e0a45b36376',
   },
 }
 
